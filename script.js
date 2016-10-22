eb5dddba9b9c1c70047e928db1567744ccdb96a2
liushi22781bob <liushi22781@hotmail.com>
2016-10-22 12:57:15 +0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ var elementContainer = $("#generated-element-container")
 $("#make-red-element").click(function() {  
   var newElement = $("<div>Hi!</div>")
   newElement.css({background: "red"})
-  
+
   elementContainer.append(newElement)
+})
+
+$("#make-blue-element").click(function() {  
+  var newElement = $("<div>Hi!</div>")
+  newElement.css({background: "red"})
+
+  elementContainer.append(newElement)
+})
+
+$("#blink-background").click(function() {
+  $("body").css({"background": "green"})
 })
