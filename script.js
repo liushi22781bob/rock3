692c4d17aeb216d2d8e7ca89d697bd07d48164f4
liushi22781bob <liushi22781@hotmail.com>
2016-10-22 13:08:43 +0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var elementContainer = $("#generated-element-container")
 
 $("#make-red-element").click(function() {  
-  var newElement = $("<div>Hello!</div>")
+  var newElement = $("<div>Hi!</div>")
   newElement.css({background: "red"})
 
   elementContainer.append(newElement)
